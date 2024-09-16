37732a7cec1d80563165a7e1563cd1e137bfabc8
Kevin Gilpin <kgilpin@gmail.com>
2024-09-16 19:21:49 -0400

    feat: analyze_patch pulls run_evaluation-*

--- analyze_patch.py
+++ analyze_patch.py
@@ -30,33 +30,62 @@ def main(instance_id: str):
     print("")
 
     solve_code_runs_dir = Path("data") / "solve_code_runs"
+    evaluate_zip_files = []
     solve_zip_files = []
     for run_dir in solve_code_runs_dir.iterdir():
         if run_dir.is_dir():
             for run_results_dir in run_dir.iterdir():
                 run_id = run_results_dir.name
-                run_solve_zip_files = list(run_results_dir.rglob("solve-*.zip"))
-                for solve_zip_file in run_solve_zip_files:
+                evaluation_solve_zip_files = list(
+                    run_results_dir.rglob("run_evaluation-*.zip")
+                )
+                for evaluate_zip_file in evaluation_solve_zip_files:
                     instances_in_zip = set()
-                    with zipfile.ZipFile(solve_zip_file, "r") as zip_ref:
+                    with zipfile.ZipFile(evaluate_zip_file, "r") as zip_ref:
+                        files = {Path(f).parts[-2] for f in zip_ref.namelist()}
+                        instances_in_zip.update(files)
+                    if instance_id in instances_in_zip:
+                        print(
+                            f"Instance Id 'run_evaluation' found in Run ID: {run_id}, File: {evaluate_zip_file}"
+                        )
+                        evaluate_zip_files.append(evaluate_zip_file)
+
+                run_solve_zip_files = list(run_results_dir.rglob("solve-*.zip"))
+                for evaluate_zip_file in run_solve_zip_files:
+                    instances_in_zip = set()
+                    with zipfile.ZipFile(evaluate_zip_file, "r") as zip_ref:
                         files = {Path(f).parts[0] for f in zip_ref.namelist()}
                         instances_in_zip.update(files)
                     if instance_id in instances_in_zip:
                         print(
-                            f"Instance id found in Run ID: {run_id}, File: {solve_zip_file}"
+                            f"Instance Id 'solve' found in Run ID: {run_id}, File: {evaluate_zip_file}"
                         )
-                        solve_zip_files.append(solve_zip_file)
+                        solve_zip_files.append(evaluate_zip_file)
 
-    for solve_zip_file in solve_zip_files:
-        run_id = solve_zip_file.parent.name
+    for evaluate_zip_file in evaluate_zip_files:
+        run_id = evaluate_zip_file.parent.name
 
-        analyze_dir = Path("analyze") / instance_id / run_id
+        analyze_dir = Path("analyze") / "run_evaluation" / run_id
+        print(f"Unpacking run evaluation logs to to {analyze_dir}")
+        analyze_dir.mkdir(exist_ok=True, parents=True)
+        shutil.rmtree(analyze_dir)
+
+        with zipfile.ZipFile(evaluate_zip_file, "r") as zip_ref:
+            # Extract the specified run_evaluation-*.json files
+            for file in zip_ref.namelist():
+                if instance_id in file:
+                    zip_ref.extract(file, analyze_dir)
+
+    for evaluate_zip_file in solve_zip_files:
+        run_id = evaluate_zip_file.parent.name
+
+        analyze_dir = Path("analyze") / "solve" / run_id
         print(f"Unpacking solve logs to to {analyze_dir}")
         print("")
         analyze_dir.mkdir(exist_ok=True, parents=True)
         shutil.rmtree(analyze_dir)
 
-        with zipfile.ZipFile(solve_zip_file, "r") as zip_ref:
+        with zipfile.ZipFile(evaluate_zip_file, "r") as zip_ref:
             # Extract the specified solve-*.json files
             for file in zip_ref.namelist():
                 if instance_id in file:
